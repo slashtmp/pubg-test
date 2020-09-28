c462f5e989e3eae216a951a68eac226976f98ce9
Tomas Persson <tomas.persson@nordlo.com>
2020-09-28 16:47:55 +0200

    Got telemetry data

--- lab/playerstats.py
+++ lab/playerstats.py
@@ -2,6 +2,8 @@ import json
 import requests
 from textwrap import indent
 
+telemetryUrl = []
+
 api_key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI3M2U0MTExMC1iNzVmLTAxMzctMGRlZS03ZjI1NmUzMjk2MWIiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTY4Mjc0NDMxLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6InRvbWFzLW0tcGVyc3NvIn0.92JDktvNaTWGl4tRgXVDD-Xpp88HDlmtG5GKQ4j9sLI"
 
 #url = "https://api.pubg.com/shards/xbox/players?filter[playerNames]=MutX"
@@ -31,9 +33,18 @@ for match in matchList[:1]:
         print( match["id"] )
         urlMatch = urlBase + "matches/" + match["id"]
         rMatch = requests.get(urlMatch, headers=header)
-        xx = rMatch.json()
-        for i in xx["included"]:
-            if( i['type'] == "asset"):
-                print( i['attributes']['URL'] )
+        
+        for i in rMatch.json()["included"]:
+            if( i['type'] == "asset" and i['attributes']['URL'] ):
+                telemetryUrl.append( i['attributes']['URL'] )
 
-        print( json.dumps(rMatch.json(), indent = 2 ) )
+telemetryHeader = {
+    "Accept": "application/vnd.api+json",
+    "Accept-Encoding": "gzip"
+}
+
+for url in telemetryUrl:
+    r = requests.get( url, telemetryHeader )
+    telemetry = r.json()
+
+    print( "loop" )
